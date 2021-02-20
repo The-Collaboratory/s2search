4dff796cf4e482d7596b81a90a61391c5ba6c34c
Branton Demoss <brantondemoss@gmail.com>
2021-02-20 23:54:23 +0000

    mp.Pool speedup

--- s2search/rank.py
+++ s2search/rank.py
@@ -1,9 +1,9 @@
 import os
 import pickle
-import kenlm
 import numpy as np
 from s2search.text import fix_text, fix_author_text
 from s2search.features import make_features, posthoc_score_adjust
+from multiprocessing import Pool, cpu_count
 
 
 class S2Ranker:
@@ -15,15 +15,11 @@ class S2Ranker:
     """
     def __init__(self, data_dir, use_posthoc_correction=True):
         self.use_posthoc_correction = use_posthoc_correction
-        self.data_dir = data_dir
-        
-        lm_title_abstracts = kenlm.Model(os.path.join(data_dir, 'titles_abstracts_lm.binary'))
-        lm_authors = kenlm.Model(os.path.join(data_dir, 'authors_lm.binary'))
-        lm_venues = kenlm.Model(os.path.join(data_dir, 'venues_lm.binary'))
-        self.lms = (lm_title_abstracts, lm_authors, lm_venues)
-
+        self.data_dir = data_dir        
         with open(os.path.join(data_dir, 'lightgbm_model.pickle'), 'rb') as f:
             self.model = pickle.load(f)
+
+        self.pool = Pool(cpu_count())
     
     def score(self, query, papers):
         """Score each pair of (query, paper) for all papers
@@ -37,10 +33,12 @@ class S2Ranker:
             scores {np.array} -- an array of scores, one per paper in papers
         """
         query = str(query)
-        X = np.array([
-            make_features(query, self.prepare_result(paper), self.lms) 
-            for paper in papers
-        ])
+        #presults = [self.prepare_result(paper) for paper in papers]
+        presults = self.pool.map(self.prepare_result,papers)
+        args = [(query,pr) for pr in presults]
+        feats = self.pool.starmap(make_features, args)
+        X = np.array(feats)
+        
         scores = self.model.predict(X)
         if self.use_posthoc_correction:
             scores = posthoc_score_adjust(scores, X, query)
